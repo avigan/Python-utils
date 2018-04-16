3c7e628ca97f5badf853c7c683e003b44e4d40a7
Arthur Vigan <arthur.vigan@lam.fr>
2018-04-16 18:45:30 +0200

    Added conditional import for ds9
    Fails on Windows because sysv_ipc package is not compatible and is therefore missing

--- vigan/utils/__init__.py
+++ vigan/utils/__init__.py
@@ -1,10 +1,15 @@
 __all__ = ['convert', 'ds9', 'fit', 'imutils', 'prof', 'sendmail']
 
 from . import convert
-from . import ds9
 from . import fit
 from . import imutils
 from . import prof
 
 from .sendmail import sendmail
 from .convert import *
+
+# ds9 module not supported in Windows
+try:
+    from . import ds9
+except ImportError:
+    print('Warning: vigan.utils.ds9 is not available in Windows')
