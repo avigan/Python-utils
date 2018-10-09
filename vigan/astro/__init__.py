df87d4c5e95828fd911b699b65e16e21fdcfcfa0
Arthur Vigan <arthur.vigan@lam.fr>
2018-10-09 16:36:16 +0200

    Updated skycalc module

--- vigan/astro/__init__.py
+++ vigan/astro/__init__.py
@@ -1,3 +1,8 @@
 __all__ = ['proper_motion']
 
 from . import proper_motion
+
+try:
+    from .skycalc import compute_sky_model
+except ImportError:
+    pass
--- vigan/astro/skycalc.py
+++ vigan/astro/skycalc.py
@@ -6,67 +6,67 @@ from astropy.io import fits
 
 def compute_sky_model(
         # observatory
-        observatory=    '2640',
+        observatory='2640',
 
         # airmass
-        airmass=           1.0,
+        airmass=1.0,
 
         # season and period of night
-        pwv_mode=        'pwv',
-        season=              0,
-        time=                0,
+        pwv_mode='pwv',
+        season=0,
+        time=0,
         
         # precipitable water vapor
-        pwv=               2.5,
+        pwv=2.5,
         
         # monthly averaged solar flux
-        msolflux=        130.0,
+        msolflux=130.0,
         
         # scattered moon light
-        incl_moon=         'Y',
-        moon_sun_sep=     90.0,
-        moon_target_sep=  45.0,
-        moon_alt=         45.0,
-        moon_earth_dist=   1.0,
+        incl_moon='Y',
+        moon_sun_sep=90.0,
+        moon_target_sep=45.0,
+        moon_alt=45.0,
+        moon_earth_dist=1.0,
         
         # star light
-        incl_starlight=    'Y',
+        incl_starlight='Y',
         
         # zodiacal light
-        incl_zodiacal=     'Y',
-        ecl_lon=         135.0,
-        ecl_lat=          90.0,
+        incl_zodiacal='Y',
+        ecl_lon=135.0,
+        ecl_lat=90.0,
         
         # molecular emission of lower atmosphere
-        incl_loweratm=     'Y',
+        incl_loweratm='Y',
         
         # molecular emission of upper atmosphere
-        incl_upperatm=     'Y',
+        incl_upperatm='Y',
         
         # airglow continuum
-        incl_airglow=      'Y',
+        incl_airglow='Y',
         
         # instrument thermal emission
-        incl_therm=        'N',
-        therm_t1=          0.0,
-        therm_e1=          0.0,
-        therm_t2=          0.0,
-        therm_e2=          0.0,
-        therm_t3=          0.0,
-        therm_e3=          0.0,
+        incl_therm='N',
+        therm_t1=0.0,
+        therm_e1=0.0,
+        therm_t2=0.0,
+        therm_e2=0.0,
+        therm_t3=0.0,
+        therm_e3=0.0,
         
         # wavelength grid
-        vacair=          'vac',
-        wmin=            900.0,
-        wmax=           3000.0,
-        wgrid_mode= 'fixed_wavelength_step',
-        wdelta=            0.1,
-        wres=            20000,
+        vacair='vac',
+        wmin=900.0,
+        wmax=3000.0,
+        wgrid_mode='fixed_wavelength_step',
+        wdelta=0.1,
+        wres=20000,
         
         # line spread function
-        lsf_type=       'none',
-        lsf_gauss_fwhm=    5.0,
-        lsf_boxcar_fwhm=   5.0):
+        lsf_type='none',
+        lsf_gauss_fwhm=5.0,
+        lsf_boxcar_fwhm=5.0):
 
     '''Compute a sky model using the ESO SkyCalc online tool
 
@@ -120,8 +120,46 @@ def compute_sky_model(
             * dflux2: sky emission +1 sigma uncertainty    
     '''
 
+    sky_dict = {
+        'observatory': observatory,
+        'airmass': airmass,
+        'pwv_mode': pwv_mode,
+        'season': season,
+        'time': time,
+        'pwv': pwv,
+        'msolflux': msolflux,
+        'incl_moon': incl_moon,
+        'moon_sun_sep': moon_sun_sep,
+        'moon_target_sep': moon_target_sep,
+        'moon_alt': moon_alt,
+        'moon_earth_dist': moon_earth_dist,
+        'incl_starlight': incl_starlight,
+        'incl_zodiacal': incl_zodiacal,
+        'ecl_lon': ecl_lon,
+        'ecl_lat': ecl_lat,
+        'incl_loweratm': incl_loweratm,
+        'incl_upperatm': incl_upperatm,
+        'incl_airglow': incl_airglow,
+        'incl_therm': incl_therm,
+        'therm_t1': therm_t1,
+        'therm_e1': therm_e1,
+        'therm_t2': therm_t2,
+        'therm_e2': therm_e2,
+        'therm_t3': therm_t3,
+        'therm_e3': therm_e3,
+        'vacair': vacair,
+        'wmin': wmin,
+        'wmax': wmax,
+        'wgrid_mode': wgrid_mode,
+        'wdelta': wdelta,
+        'wres': wres,
+        'lsf_type': lsf_type,
+        'lsf_gauss_fwhm': lsf_gauss_fwhm,
+        'lsf_boxcar_fwhm': lsf_boxcar_fwhm
+    }
+    
     sky_model = SkyCalc.SkyModel()
-    sky_model.callwith(skydict)
+    sky_model.callwith(sky_dict)
 
     b = io.BytesIO(sky_model.getdata())
     data = fits.getdata(b)
