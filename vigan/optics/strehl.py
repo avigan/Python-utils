411f146900dec669efa3d77ae4d95b966abd8fb6
Arthur Vigan <arthur.vigan@lam.fr>
2019-05-13 20:27:07 +0200

    Added possibility to have VLT pupil and SPHERE apodizers for Strehl computation

--- vigan/optics/strehl.py
+++ vigan/optics/strehl.py
@@ -2,14 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import scipy.fftpack as fft
+import scipy.interpolate as interp
+import vigan
 
 from ..utils import imutils
 from . import aperture
 
+from pathlib import Path
+from astropy.io import fits
+
 
 def strehl(img, sampling, center=True, rebin=2,
            background_fit=True, background_fit_order=2, pixel_tf=True,
-           central_obscuration=0, disp=False, ymin=1e-4):
+           pupil_shape='circular', central_obscuration=0, apodizer='none',
+           disp=False, ymin=1e-4):
     '''
     Compute Strehl ratio estimation from a PSF image
 
@@ -36,8 +42,16 @@ def strehl(img, sampling, center=True, rebin=2,
     pixel_tf : bool
         Taken into account the pixel transfer function. Default is True
 
+    pupil_shape : str
+        Type of pupil. Possible values are circular or vlt. Default is circular
+
     central_obscuration : float
-        Value of the central obscuration. Default is 0
+        Value of the central obscuration. Default is 0. Does not apply if
+        pupil_shape='vlt'
+
+    apodizer : str
+        Apodizer name. Possible values are none, sphere-apo1, sphere-apo2, 
+        and sphere-sllc. Default is none
 
     disp : bool
         Display a summary plot. Default is False
@@ -86,7 +100,7 @@ def strehl(img, sampling, center=True, rebin=2,
     if background_fit:
         # background subtraction using a linear fit on the first OTF
         # points
-        otf_1d, r = imutils.profile(otf, type='mean', step=1, exact=False)
+        otf_1d, r = imutils.profile(otf, ptype='mean', step=1, exact=False)
 
         dimfit = background_fit_order + 2
         u = np.arange(1, dimfit+1, dtype=np.float)
@@ -118,8 +132,8 @@ def strehl(img, sampling, center=True, rebin=2,
 
     # frequencies larger than D/lambda are set to zero
     rt = (dim - 1) / sampling * 2
-    mask = aperture.disc(dim, int(rt//2), diameter=False, cpix=True)
-
+    mask = aperture.disc(dim, int(rt//2), diameter=False, strict=False, cpix=True)
+        
     # divide by pixel transfer function to account for spatial frequencies
     # over one pixel
     otf_corr = otf_corr / pix_tf * mask
@@ -129,8 +143,38 @@ def strehl(img, sampling, center=True, rebin=2,
 
     # ideal pupil
     rr = np.ceil(dim / (sampling))
-    pupil = aperture.disc_obstructed(dim, int(rr), central_obscuration, diameter=True)
+    if pupil_shape.lower() == 'circular':
+        pupil = aperture.disc_obstructed(dim, int(rr), central_obscuration, diameter=True, strict=False, cpix=True)
+    elif pupil_shape.lower() == 'vlt':
+        pupil = aperture.vlt_pupil(dim, int(rr), dead_actuators=None, strict=False, cpix=True)
+    else:
+        raise ValueError('Unknown pupil shape {}'.format(pupil_shape))
 
+    # apodizer
+    if apodizer.lower() == 'none':
+        apod = np.ones_like(pupil)
+    else:
+        if apodizer.lower() == 'sphere-apo1':
+            apod_big = fits.getdata(Path(vigan.__file__).parent / 'data' / 'sphere' / 'sphere_APO1_field_transmission_map.fits')
+        elif apodizer.lower() == 'sphere-apo2':
+            apod_big = fits.getdata(Path(vigan.__file__).parent / 'data' / 'sphere' / 'sphere_APO2_field_transmission_map.fits')
+        elif apodizer.lower() == 'sphere-sllc':
+            apod_big = fits.getdata(Path(vigan.__file__).parent / 'data' / 'sphere' / 'sphere_SLLC_field_transmission_map.fits')
+        else:
+            raise ValueError('Unknown apodizer {}'.format(apodizer))
+        
+        d = apod_big.shape[-1]
+        p = apod_big[d//2-1, d//2-1:]        
+        r = np.arange(p.size) / (p.size-1)
+        
+        rho, theta = aperture.coordinates(dim, int(rr), diameter=True, strict=False, cpix=True, polar=True)
+        
+        interp_fun = interp.interp1d(r, p)
+        apod = interp_fun(rho)
+        apod = np.nan_to_num(apod)
+    
+    pupil = pupil*apod
+        
     # pupil autocorrelation
     otf_pupil = fft.fftshift(fft.fft2(np.abs(fft.ifft2(pupil))**2).real)
     otf_pupil = otf_pupil / otf_pupil.max()
@@ -141,9 +185,9 @@ def strehl(img, sampling, center=True, rebin=2,
     # display result
     if disp:
         otf = otf / otf.max()
-        otf_1d, r_otf = imutils.profile(otf, type='mean', step=1, rmax=dim//2-1)
-        otf_corr_1d, r = imutils.profile(otf_corr, type='mean', step=1, rmax=dim//2-1)
-        otf_pupil_1d, r = imutils.profile(otf_pupil, type='mean', step=1, rmax=dim//2-1)
+        otf_1d, r_otf = imutils.profile(otf, ptype='mean', step=1, rmax=dim//2-1)
+        otf_corr_1d, r = imutils.profile(otf_corr, ptype='mean', step=1, rmax=dim//2-1)
+        otf_pupil_1d, r = imutils.profile(otf_pupil, ptype='mean', step=1, rmax=dim//2-1)
 
         r_otf = r_otf / (dim//2 - 1) * sampling / 2
 
@@ -154,7 +198,7 @@ def strehl(img, sampling, center=True, rebin=2,
         plt.semilogy(r_otf, otf_pupil_1d, lw=2, linestyle='-.', label='TF pupil')
 
         if pixel_tf:
-            otf_pixel_1d, r = imutils.profile(pix_tf, type='mean', step=1, rmax=dim//2-1)
+            otf_pixel_1d, r = imutils.profile(pix_tf, ptype='mean', step=1, rmax=dim//2-1)
             plt.semilogy(r_otf, otf_pixel_1d, lw=2, linestyle=':', label='TF pupil')
 
         plt.text(0.5, 0.95, 'Sr = {:.2f}%'.format(strehl*100), transform=plt.gca().transAxes,
