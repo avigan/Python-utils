f58d61516114d0e08017fcea0f62cfddf1b79fe4
Arthur Vigan <arthur.vigan@lam.fr>
2019-06-13 18:41:37 +0200

    Simplified the _read_model_sonora() function
    Models are now pre-formated in csv files, like the BEX models

--- vigan/astro/evolution.py
+++ vigan/astro/evolution.py
@@ -229,170 +229,18 @@ def _read_model_sonora(path, fname, instrument):
         Numpy data array
     '''
 
-    # for SONORA, magnitudes are calculated for different observatories or instruments
-    observatory = instrument
+    df = pd.read_csv(path / fname, index_col=(0, 1))
 
-    ####################################################
-    # magnitudes
-    #
+    masses  = np.sort(np.unique(df.index.get_level_values(0)))  # MJup
+    ages    = np.sort(np.unique(df.index.get_level_values(1)))  # yr
+    values  = df.columns
 
-    # read observatory and filter header
-    re_obs = re.compile('\s+\|')
-    file = open(path / fname, 'r')
-    for line in file:
-        m = re_obs.match(line)
-        if (m is not None):
-            break
-    obs  = line
-    filt = file.readline()
-    file.close()
-
-    # associate observatory and filters
-    obs_list  = []
-    filt_list = []
-    obs_all   = obs.split('|')
-    start = 0
-    for obs in obs_all:
-        sub_filt = filt[start:start+len(obs)]
-        start = start+len(obs)+1
-
-        obs_short = obs.strip().lower()
-        all_filt  = sub_filt.split()
-
-        if len(all_filt) != 0:
-            filt_list.extend(all_filt)
-            obs_list.extend([obs_short for i in range(len(all_filt))])
-
-    # read general data
-    data_mag = pd.read_csv(path / fname, sep='\s+', skiprows=11, header=None)
-    data_mag.columns = pd.MultiIndex.from_arrays((obs_list, filt_list))
-
-    Teff_mag = data_mag[('', 'Teff')].unique()
-    logg_mag = data_mag[('', 'log_g')].unique()
-
-    nTeff_mag = len(Teff_mag)
-    nlogg_mag = len(logg_mag)
-
-    ####################################################
-    # evolution
-    #
-    path_evol = path / 'sonora_evolution_files'
-    files = path_evol.glob('model_seq.*')
-    files = list(files)
-
-    # column names
-    names = ('idx', 't(years)', 'log L', 'R(cm)', 'Ts', 'Te', 'log rc',
-             'log Pc', 'log Tc', 'g', 'Uth', 'Ugrav', 'log Lnuc', 'I')
-
-    # add filters
-    sub = data_mag.loc[:, (observatory, slice(None))]
-    filters = np.array(sub.columns.get_level_values(1))
-
-    # values
-    values = ['Teff', 'logL', 'radius', 'logg']
-    values.extend(filters)
-    values = np.array(values)
-
-    # ages
-    ages = np.logspace(4, 10, 100)
-
-    # pseudo-masses
-    pmasses = []
-    for file in files:
-        mass = np.round(float('0{}'.format(file.suffix))*10000).astype(int)
-        pmasses.append(mass)
-    pmasses = sorted(pmasses)
-
-    # real masses in Mjup
-    re_mass = re.compile('\*\*\* MS=\s+([0-9]+\.[0-9]+) Mj')
-    masses = np.zeros(len(pmasses))
-    for idx, pmass in enumerate(pmasses):
-        fname = path_evol / 'model_seq.{:04}'.format(pmass)
-        file  = open(fname)
-        for line in file:
-            m = re_mass.match(line)
-            if (m is not None):
-                masses[idx] = float(m[1])
-                break
-        file.close()
-
-    # read all data
-    data = np.full((len(pmasses), len(ages), len(values)), np.nan)
-    for filt in filters:
-        print(' * {} - {}'.format(observatory, filt))
-        aTeff = data_mag[('', 'Teff')].values.reshape(nlogg_mag, nTeff_mag)
-        alogg = data_mag[('', 'log_g')].values.reshape(nlogg_mag, nTeff_mag)
-        amag  = data_mag[(observatory, filt)].values.reshape(nlogg_mag, nTeff_mag)
-        fint_mag = interp.Rbf(alogg, aTeff, amag, function='linear', smooth=0)
-    
-        Teff_min = aTeff.min()
-        Teff_max = aTeff.max()
-        logg_min = alogg.min()
-        logg_max = alogg.max()
-
-        for idx, pmass in enumerate(pmasses):
-            # print(' * {:6.4f} Msun'.format(pmass/10000))
-            fname = path_evol / 'model_seq.{:04}'.format(pmass)
-
-            # data
-            df = pd.read_csv(fname, sep='\s+', skiprows=13, header=None, index_col=0, names=names)
-            x = df['t(years)']
-
-            #
-            # bulk physical parameters
-            #
-
-            # Teff
-            y = df['Te']
-            fint  = interp.interp1d(x, y, bounds_error=False, fill_value=np.nan)    
-            data[idx, :, values == 'Teff'] = fint(ages)
-
-            # logL
-            y = df['log L']
-            fint  = interp.interp1d(x, y, bounds_error=False, fill_value=np.nan)    
-            data[idx, :, values == 'logL'] = fint(ages)
-
-            # logL
-            y = df['R(cm)']*units.cm / cst.R_jup.to(units.cm)
-            fint  = interp.interp1d(x, y, bounds_error=False, fill_value=np.nan)    
-            data[idx, :, values == 'radius'] = fint(ages)
-
-            # logg
-            y = np.log10(df['g'])
-            fint  = interp.interp1d(x, y, bounds_error=False, fill_value=np.nan)    
-            data[idx, :, values == 'logg'] = fint(ages)
-
-            #
-            # magnitudes
-            #
-            cTeff = data[idx, :, values == 'Teff'].squeeze()
-            clogg = data[idx, :, values == 'logg'].squeeze()
-            for i, (t, l) in enumerate(zip(cTeff, clogg)):
-                data[idx, i, values == filt] = fint_mag(l, t)
-            mask = (cTeff < Teff_min) | (cTeff > Teff_max) | (clogg < logg_min) | (clogg > logg_max)
-            data[idx, mask, values == filt] = np.nan
+    data = np.zeros((len(masses), len(ages), len(values)))
+    for iv, val in enumerate(values):
+        for im, mass in enumerate(masses):
+            tmp = df.loc[(mass, slice(None)), val]
+            data[im, :, iv] = tmp
             
-            # plot
-            # plt.figure(0, figsize=(18, 7))
-            # plt.clf()
-            # plt.subplot(121)            
-            # plt.pcolor(aTeff, alogg, amag)
-            # cs = plt.contour(aTeff, alogg, amag, colors='k', levels=np.arange(100))
-            # plt.clabel(cs, inline=False, fontsize='x-small', fmt='%.0f')
-            # plt.plot(cTeff, clogg, marker='+', color='r', linestyle='-')
-            # plt.xlabel('Teff [k]')
-            # # plt.xlim(aTeff.min(), aTeff.max())
-            # plt.ylabel('log(g) [dex cgs]')
-            # # plt.ylim(alogg.min(), alogg.max())
-            # plt.title('Mass = {} Msun'.format(pmass/1e4))
-
-            # plt.subplot(122)
-            # plt.semilogx(ages, data[idx, :, values == filt].squeeze(), marker='+')
-            # plt.xlabel('Age [Myr]')
-            # plt.ylabel('{} [mag]'.format(filt))
-            # plt.title('Mass = {} Msun'.format(pmass/1e4))
-            # plt.tight_layout()            
-
     # converts ages in Myr
     ages = ages / 1e6
     
@@ -726,20 +574,22 @@ models = {
         {'instrument': 'irdis',  'name': 'cond2003',            'file': 'model.AMES-Cond-2003.M-0.0.SPHERE.Vega',    'function': _read_model_PHOENIX_websim},    
         {'instrument': 'irdis',  'name': 'bhac2015+dusty2000',  'file': 'BHAC15_DUSTY00_iso_t10_10.SPHERE',          'function': _read_model_BHAC2015},
         {'instrument': 'irdis',  'name': 'bhac2015+cond2003',   'file': 'BHAC15_COND03_iso_t10_10.SPHERE',           'function': _read_model_BHAC2015},
-        {'instrument': 'mko',    'name': 'sonora',              'file': 'sonora_mag_table.dat',                      'function': _read_model_sonora},
-        {'instrument': '2mass',  'name': 'sonora',              'file': 'sonora_mag_table.dat',                      'function': _read_model_sonora},
-        {'instrument': 'keck',   'name': 'sonora',              'file': 'sonora_mag_table.dat',                      'function': _read_model_sonora},
-        {'instrument': 'sdss',   'name': 'sonora',              'file': 'sonora_mag_table.dat',                      'function': _read_model_sonora},
-        {'instrument': 'irac',   'name': 'sonora',              'file': 'sonora_mag_table.dat',                      'function': _read_model_sonora},
-        {'instrument': 'wise',   'name': 'sonora',              'file': 'sonora_mag_table.dat',                      'function': _read_model_sonora},        
-        {'instrument': 'irdis',  'name': 'bex_cond_coldest',    'file': 'bex_ames-cond_coldest.csv',                 'function': _read_model_bex},
-        {'instrument': 'irdis',  'name': 'bex_cond_warm',       'file': 'bex_ames-cond_warm.csv',                    'function': _read_model_bex},
-        {'instrument': 'irdis',  'name': 'bex_cond_hot',        'file': 'bex_ames-cond_hot.csv',                     'function': _read_model_bex},
-        {'instrument': 'irdis',  'name': 'bex_cond_hottest',    'file': 'bex_ames-cond_hottest.csv',                 'function': _read_model_bex},
-        {'instrument': 'irdis',  'name': 'bex_dusty_coldest',   'file': 'bex_ames-dusty_coldest.csv',                'function': _read_model_bex},
-        {'instrument': 'irdis',  'name': 'bex_dusty_warm',      'file': 'bex_ames-dusty_warm.csv',                   'function': _read_model_bex},
-        {'instrument': 'irdis',  'name': 'bex_dusty_hot',       'file': 'bex_ames-dusty_hot.csv',                    'function': _read_model_bex},
-        {'instrument': 'irdis',  'name': 'bex_dusty_hottest',   'file': 'bex_ames-dusty_hottest.csv',                'function': _read_model_bex}
+        
+        {'instrument': 'mko',    'name': 'sonora',              'file': 'sonora_mko.csv.gz',                         'function': _read_model_sonora},
+        {'instrument': '2mass',  'name': 'sonora',              'file': 'sonora_2mass.csv.gz',                       'function': _read_model_sonora},
+        {'instrument': 'keck',   'name': 'sonora',              'file': 'sonora_keck.csv.gz',                        'function': _read_model_sonora},
+        {'instrument': 'sdss',   'name': 'sonora',              'file': 'sonora_sdss.csv.gz',                        'function': _read_model_sonora},
+        {'instrument': 'irac',   'name': 'sonora',              'file': 'sonora_irac.csv.gz',                        'function': _read_model_sonora},
+        {'instrument': 'wise',   'name': 'sonora',              'file': 'sonora_wise.csv.gz',                        'function': _read_model_sonora},        
+        
+        {'instrument': 'irdis',  'name': 'bex_cond_coldest',    'file': 'bex_ames-cond_coldest.csv.gz',              'function': _read_model_bex},
+        {'instrument': 'irdis',  'name': 'bex_cond_warm',       'file': 'bex_ames-cond_warm.csv.gz',                 'function': _read_model_bex},
+        {'instrument': 'irdis',  'name': 'bex_cond_hot',        'file': 'bex_ames-cond_hot.csv.gz',                  'function': _read_model_bex},
+        {'instrument': 'irdis',  'name': 'bex_cond_hottest',    'file': 'bex_ames-cond_hottest.csv.gz',              'function': _read_model_bex},
+        {'instrument': 'irdis',  'name': 'bex_dusty_coldest',   'file': 'bex_ames-dusty_coldest.csv.gz',             'function': _read_model_bex},
+        {'instrument': 'irdis',  'name': 'bex_dusty_warm',      'file': 'bex_ames-dusty_warm.csv.gz',                'function': _read_model_bex},
+        {'instrument': 'irdis',  'name': 'bex_dusty_hot',       'file': 'bex_ames-dusty_hot.csv.gz',                 'function': _read_model_bex},
+        {'instrument': 'irdis',  'name': 'bex_dusty_hottest',   'file': 'bex_ames-dusty_hottest.csv.gz',             'function': _read_model_bex}
     ],
     'data': {}
 }
