9992a4098e01570e4d8904a2732081c3aacdc2c0
Arthur Vigan <arthur.vigan@lam.fr>
2020-07-29 17:40:09 +0200

    Add confidence_interval_statistics() function

--- vigan/utils/misc.py
+++ vigan/utils/misc.py
@@ -53,3 +53,50 @@ def monotonic_sublists(lst):
         ret_v.append(sub_v)
         
     return ret_i, ret_v
+
+def confidence_interval_statistics(data, CI=[0.99, 0.95, 0.68]):
+    '''
+    Confidence interval statistics on a set of 2D data
+
+    Parameters
+    ----------
+    data : array
+        An array of data for which the statistics must be computed
+
+    CI : list
+        List of confidence intervals (values between 0 and 1)
+
+    Returns
+    -------
+    data_med : array
+        Median of the data points
+
+    data_lim : array
+        Limits of the data for the requested confidence intervals
+    '''
+    
+    # confidence intervals
+    CI = np.array(CI)
+
+    # lengths
+    nci  = len(CI)
+    nval = data.shape[0]
+    ny   = data.shape[1]
+    
+    data_med = np.zeros(ny)
+    data_lim = np.zeros((nci, 2, ny))
+    for y in range(ny):
+        values = np.sort(data[:, y])
+
+        cmed = np.median(values)
+        data_med[y] = cmed
+
+        for c in range(len(CI)):
+            imean = np.argmin(np.abs(values - cmed))
+            cmin  = values[int(imean - nval*CI[c]/2)]
+            cmax  = values[int(imean + nval*CI[c]/2)]
+
+            data_lim[c, 0, y] = cmin
+            data_lim[c, 1, y] = cmax
+
+    return data_med, data_lim
