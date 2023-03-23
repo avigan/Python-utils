4a73dc6809d8b5f3a33a065933a6440e31f0c1d1
Arthur Vigan <arthur.vigan@lam.fr>
2023-03-23 14:36:56 +0100

    Add gaussian fit

--- vigan/utils/fit.py
+++ vigan/utils/fit.py
@@ -57,6 +57,74 @@ def circle(x, y):
     
     return cx, cy, radius
 
+def gaussian(img, window=0, edges=0, mask=None)
+    '''Gaussian fit
+    
+    Parameters
+    ----------
+    img : array
+        Image where the peak must be found
+     
+    window : int
+        Half-size of sub-window in which to do the fit. If 0, then the fit
+        is performed over the full image. Default is 0
+
+    edges : int
+        Number of pixels to hide on the edges. Default is 0
+
+    mask : array
+        Mask to apply to the data. Default is None
+
+    Returns
+    -------
+    params : ?
+        Gaussian fit parameters
+    '''
+
+    img = img.copy()
+    
+    # hide edges
+    if edges > 0:
+        img[0:edges, :] = 0
+        img[-edges:, :] = 0
+        img[:, 0:edges] = 0
+        img[:, -edges:] = 0
+
+    # apply mask
+    if mask is not None:
+        img *= mask
+
+    # estimate peak position    
+    imax = np.unravel_index(np.argmax(img), img.shape)
+    cx_int = imax[1]
+    cy_int = imax[0]
+
+    # sub-window
+    win = window // 2
+    if window > 0:        
+        if (window % 2):
+            raise ValueError('window parameter must be even')
+        
+        sub = img[cy_int-win:cy_int+win, cx_int-win:cx_int+win]
+
+        cx_init = win
+        cy_init = win
+    else:
+        sub = img
+ 
+        cx_init = cx_int
+        cy_init = cy_int
+
+    # fit
+    x, y = np.meshgrid(np.arange(sub.shape[1]), np.arange(sub.shape[0]))
+    g_init = models.Gaussian2D(amplitude=sub.max(), x_mean=cx_init, y_mean=cy_init) + \
+                                          models.Const2D(amplitude=0)
+    
+    fit_g = fitting.LevMarLSQFitter()
+    g = fit_g(g_init, x, y, sub)
+    
+    return g
+
 
 def peak_center(img, window=0, edges=0, mask=None):
     '''Determine the center position of a peak 
